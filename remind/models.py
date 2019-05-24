349fa4fb3cfbef3627d99a7ad0165523ca93ff80
Joseph Peterson <Joseph.Peterson@da2nd.state.nm.us>
2019-05-24 13:29:15 -0600

    Added models

--- remind/models.py
+++ remind/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class Deadline(models.Model):
+    FFA = 0
+    SCHEDULING_ORDER = 1
+    WITNESS_LIST = 2
+    REQUEST_PTI = 3
+    CONDUCT_PTI = 4
+    WITNESS_PTI = 5
+    SCIENTIFIC_EVIDENCE = 6
+    PRETRIAL_MOTION_FILING = 7
+    PRETRIAL_MOTION_RESPONSE = 8
+    PRETRIAL_MOTION_HEARING = 9
+    PRETRIAL_CONFERENCE = 10
+    FINAL_WITNESS_LIST = 11
+    NEED_FOR_INTERPRETER = 12
+    PLEA_AGREEMENT = 13
+    TRIAL = 14
+
+    TYPE_CHOICES = (
+        (FFA, 'FFA'),
+        (SCHEDULING_ORDER, 'Scheduling Order'),
+        (WITNESS_LIST, 'Witness List'),
+        (REQUEST_PTI, 'Request PTIs'),
+        (CONDUCT_PTI, 'Conduct Initial PTIs'),
+        (WITNESS_PTI, 'Winess PTIs'),
+        (SCIENTIFIC_EVIDENCE, 'Scientific Evidence'),
+        (PRETRIAL_MOTION_FILING, 'Pretrial Motion Filing'),
+        (PRETRIAL_MOTION_RESPONSE, 'Pretrial Motion Response'),
+        (PRETRIAL_MOTION_HEARING, 'Pretrial Motion Hearing'),
+        (PRETRIAL_CONFERENCE, 'Pretrial Conference'),
+        (FINAL_WITNESS_LIST, 'Final Witness List'),
+        (NEED_FOR_INTERPRETER, 'Need for Interpreter'),
+        (PLEA_AGREEMENT, 'Plea Agreement'),
+        (TRIAL, 'Trial'),
+    )
+
+    type = models.IntegerField(choices=TYPE_CHOICES)
+    case = models.ForeignKey(Case, on_delete=models.PROTECT)
+    date = models.DateTimeField()
+    expired = models.BooleanField(default=False)
+
+
+class Case(models.Model):
+    TRACK_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3')
+    )
+
+    case_number = models.CharField(max_length=20)
+    track = models.IntegerField(choices=TRACK_CHOICES, null=True, blank=True)
+    attorney_firstname = models.CharField(max_length=60)
+    attorney_lastname = models.CharField(max_length=60)
+
+    arraignment_date = models.DateTimeField(null=True, blank=True)
+    scheduling_order_date = models.DateTimeField(null=True, blank=True)
+    pti_request_date = models.DateTimeField(null=True, blank=True)
+    trial_date = models.DateTimeField(null=True, blank=True)
+    pass
+
+
+class Motion(models.Model):
+    # type
+    pass
