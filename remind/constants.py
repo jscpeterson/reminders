a0148b7e1b55ac76d141a75301a171d93618e65e
Joseph Peterson <joseph.peterson@da2nd.state.nm.us>
2019-05-24 23:33:53 +0000

    Imported utility code  from legal-beagle

---
+++ remind/constants.py
@@ -0,0 +1,66 @@
+""" Constants used by Adult Felony processes """
+from .models import Deadline
+
+SCHEDULING_ORDER_DEADLINE_DAYS = 30
+
+LAST_DAY_HOUR = 23
+LAST_DAY_MINUTE = 59
+LAST_DAY_SECOND = 59
+MIN_DAYS_FOR_DEADLINES = 11
+SATURDAY = 5
+SUNDAY = 6
+
+TRACK_ONE_DEADLINE_LIMITS = {
+    str(Deadline.TRIAL): 210,
+    'trial_extended': 240,
+    str(Deadline.WITNESS_LIST): 25,  # Days after arraignment
+    str(Deadline.REQUEST_PTI): 14,  # Days after scheduling order
+    str(Deadline.CONDUCT_PTI): 30,  # Days after PTI request
+    str(Deadline.WITNESS_PTI): 60,  # Days before trial
+    str(Deadline.SCIENTIFIC_EVIDENCE): 120,  # Days before trial
+    'scientific_evidence_extended': 90,  # Days before trial
+    str(Deadline.PRETRIAL_MOTION_FILING): 50,  # Days before trial
+    str(Deadline.PRETRIAL_MOTION_RESPONSE): 40,  # Days before trial OR 10 days after filing
+    str(Deadline.PRETRIAL_MOTION_HEARING): 35,  # Days before trial
+    str(Deadline.PRETRIAL_CONFERENCE): 15,  # Days before trial
+    str(Deadline.FINAL_WITNESS_LIST): 15,  # Days before trial
+    str(Deadline.NEED_FOR_INTERPRETER): 15,  # Days before trial
+    str(Deadline.PLEA_AGREEMENT): 10,  # Days before trial
+}
+
+TRACK_TWO_DEADLINE_LIMITS = {
+    str(Deadline.TRIAL): 300,
+    'trial_extended': 360,
+    str(Deadline.WITNESS_LIST): 25,  # Days after arraignment
+    str(Deadline.REQUEST_PTI): 21,  # Days after scheduling order
+    str(Deadline.CONDUCT_PTI): 45,  # Days after PTI request
+    str(Deadline.WITNESS_PTI): 75,  # Days before trial
+    str(Deadline.SCIENTIFIC_EVIDENCE): 120,  # Days before trial
+    'scientific_evidence_extended': 90,  # Days before trial
+    str(Deadline.PRETRIAL_MOTION_FILING): 60,  # Days before trial
+    str(Deadline.PRETRIAL_MOTION_RESPONSE): 45,  # Days before trial OR 10 days after filing
+    str(Deadline.PRETRIAL_MOTION_HEARING): 35,  # Days before trial
+    str(Deadline.PRETRIAL_CONFERENCE): 15,  # Days before trial
+    str(Deadline.FINAL_WITNESS_LIST): 15,  # Days before trial
+    str(Deadline.NEED_FOR_INTERPRETER): 15,  # Days before trial
+    str(Deadline.PLEA_AGREEMENT): 10,  # Days before trial
+}
+
+TRACK_THREE_DEADLINE_LIMITS = {
+    str(Deadline.TRIAL): 455,
+    'trial_extended': 545,
+    str(Deadline.TRIAL): 210,
+    str(Deadline.WITNESS_LIST): 25,  # Days after arraignment
+    str(Deadline.REQUEST_PTI): 21,  # Days after scheduling order
+    str(Deadline.CONDUCT_PTI): 60,  # Days after PTI request
+    str(Deadline.WITNESS_PTI): 100,  # Days before trial
+    str(Deadline.SCIENTIFIC_EVIDENCE): 150,  # Days before trial
+    'scientific_evidence_extended': 120,  # Days before trial
+    str(Deadline.PRETRIAL_MOTION_FILING): 70,  # Days before trial
+    str(Deadline.PRETRIAL_MOTION_RESPONSE): 55,  # Days before trial OR 10 days after filing
+    str(Deadline.PRETRIAL_MOTION_HEARING): 45,  # Days before trial
+    str(Deadline.PRETRIAL_CONFERENCE): 20,  # Days before trial
+    str(Deadline.FINAL_WITNESS_LIST): 20,  # Days before trial
+    str(Deadline.NEED_FOR_INTERPRETER): 15,  # Days before trial
+    str(Deadline.PLEA_AGREEMENT): 10,  # Days before trial
+}
--- remind/tests.py
+++ remind/tests.py
@@ -1,3 +1,96 @@
 from django.test import TestCase
+from datetime import datetime
+from . import utils
+from .constants import LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND
 
-# Create your tests here.
+
+class TestActualDeadline(TestCase):
+
+    def test_long_deadline_from_start(self):
+        # Test cases of 60 day deadlines
+        # Deadline from test case T-4-FR-2019000277
+        event = datetime(2019, 1, 18, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Date of preventive detention
+        days = 60
+        result = datetime(2019, 3, 19, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # 60 day deadline for arraignment
+        self.assertEqual(utils.get_actual_deadline_from_start(event, days), result)
+
+        # Deadline from test case T-4-FR-2018004144
+        event = datetime(2018, 8, 2, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Date of preventive detention
+        days = 60
+        result = datetime(2018, 10, 1, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # 60 day deadline for arraignment
+        self.assertEqual(utils.get_actual_deadline_from_start(event, days), result)
+
+    def test_weekend_deadline_from_start(self):
+        # Test case in which a 10 day deadline overlapped with a weekend, but no holidays
+        # Deadline from test case T-4-FR-2018004144
+        event = datetime(2018, 8, 2, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Date of preventive detention
+        days = 10
+        result = datetime(2018, 8, 16, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # 10 day deadline for arraignment
+        self.assertEqual(utils.get_actual_deadline_from_start(event, days), result)
+
+    def test_holiday_deadline_from_start(self):
+        # Test case in which a 10 day deadline overlapped with weekends and a holiday, deadline landing on a weekend
+        # Deadline from test case T-4-FR-2019000277
+        event = datetime(2019, 1, 18, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Date of preventive detention
+        days = 10
+        result = datetime(2019, 2, 4, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # 10 day deadline for arraignment
+        self.assertEqual(utils.get_actual_deadline_from_start(event, days), result)
+
+    def test_long_deadline_from_end(self):
+        # Test case of 15 day deadline from end date event
+        # Using status hearing of D-202-CR-2019-00568 as reference
+        event = datetime(2019, 10, 21, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Date of trial
+        days = 15
+        result = datetime(2019, 10, 4, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Latest possible deadline for witness list/language access services
+        self.assertEqual(utils.get_actual_deadline_from_end(event, days), result)
+
+    def test_holiday_deadline_from_end(self):
+        # Test case of 10 day deadline from end date event, overlapping with weekends and holidays
+        # Using status hearing of D-202-CR-2019-00568 as reference
+        event = datetime(2019, 10, 21, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Date of trial
+        days = 10
+        result = datetime(2019, 10, 4, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)  # Latest possible deadline for submitting a plea deal
+        self.assertEqual(utils.get_actual_deadline_from_end(event, days), result)
+
+
+class TestDeadlineCheck(TestCase):
+
+    def test_long_prior_event(self):
+        event = datetime(2018, 8, 2, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        days = 60
+        deadline = datetime(2018, 10, 1, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        good_deadline = datetime(2018, 10, 2, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        bad_deadline = datetime(2018, 9, 28, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        self.assertTrue(utils.is_deadline_within_limits(deadline=deadline, event=event, days=days, future_event=False))
+        self.assertTrue(utils.is_deadline_within_limits(deadline=good_deadline, event=event, days=days, future_event=False))
+        self.assertFalse(utils.is_deadline_within_limits(deadline=bad_deadline, event=event, days=days, future_event=False))
+
+    def test_10day_prior_event(self):
+        event = datetime(2018, 8, 2, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        days = 10
+        deadline = datetime(2018, 8, 16, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        good_deadline = datetime(2018, 8, 17, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        bad_deadline = datetime(2018, 8, 15, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        self.assertTrue(utils.is_deadline_within_limits(deadline=deadline, event=event, days=days, future_event=False))
+        self.assertTrue(utils.is_deadline_within_limits(deadline=good_deadline, event=event, days=days, future_event=False))
+        self.assertFalse(utils.is_deadline_within_limits(deadline=bad_deadline, event=event, days=days, future_event=False))
+
+    def test_long_future_event(self):
+        event = datetime(2019, 10, 21, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        days = 15
+        deadline = datetime(2019, 10, 4, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        good_deadline = datetime(2019, 10, 3, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        bad_deadline = datetime(2019, 10, 7, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        self.assertTrue(utils.is_deadline_within_limits(deadline=deadline, event=event, days=days, future_event=True))
+        self.assertTrue(utils.is_deadline_within_limits(deadline=good_deadline, event=event, days=days, future_event=True))
+        self.assertFalse(utils.is_deadline_within_limits(deadline=bad_deadline, event=event, days=days, future_event=True))
+
+    def test_10day_future_event(self):
+        event = datetime(2019, 10, 21, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        days = 10
+        deadline = datetime(2019, 10, 4, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        good_deadline = datetime(2019, 10, 3, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        bad_deadline = datetime(2019, 10, 7, LAST_DAY_HOUR, LAST_DAY_MINUTE, LAST_DAY_SECOND)
+        self.assertTrue(utils.is_deadline_within_limits(deadline=deadline, event=event, days=days, future_event=True))
+        self.assertTrue(utils.is_deadline_within_limits(deadline=good_deadline, event=event, days=days, future_event=True))
+        self.assertFalse(utils.is_deadline_within_limits(deadline=bad_deadline, event=event, days=days, future_event=True))
