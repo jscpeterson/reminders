0c525ce6c36d50ea1e0a4272b8f8aa4a7bc9f411
Joseph Peterson <joseph.peterson@da2nd.state.nm.us>
2019-05-28 21:07:20 +0000

    Figured out base template. The django-material includes did not work. Had to put in css and js links manually.

--- remind/views.py
+++ remind/views.py
@@ -1,10 +1,66 @@
-# from django.urls import reverse
-from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.edit import CreateView, FormView
 from .models import Case
-from .forms import CaseForm
+from .forms import CaseForm, SchedulingForm, TrackForm, TrialForm, OrderForm
 
 
 class CaseCreate(CreateView):
     model = Case
     form_class = CaseForm
-    success_url = '/remind/create/'
+
+    def get_success_url(self):
+        return reverse('scheduling', kwargs={'case_num': self.object.case_number})
+
+
+class SchedulingView(FormView):
+
+    template_name = 'remind/scheduling_form.html'
+    form_class = SchedulingForm
+
+    def get_form_kwargs(self):
+        return self.kwargs
+
+    def __init__(self, *args, **kwargs):
+        print('init')
+        super().__init__(*args, **kwargs)
+
+    def get_success_url(self):
+        print('success')
+        return reverse('track', kwargs={'case_num': self.kwargs['case_num']})
+
+    def form_valid(self, form):
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class TrackView(FormView):
+    template_name = 'remind/track_form.html'
+    form_class = TrackForm
+
+    def get_form_kwargs(self):
+        return self.kwargs
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class TrialView(FormView):
+    template_name = 'remind/trial_form.html'
+    form_class = TrialForm
+
+    def get_form_kwargs(self):
+        return self.kwargs
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class OrderView(FormView):
+    template_name = 'remind/order_form.html'
+    form_class = OrderForm
+
+    def get_form_kwargs(self):
+        return self.kwargs
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
