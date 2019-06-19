c58a88289ab16443c441a242caba8f9d7ede3f7d
Joseph Peterson <jscpeterson@gmail.com>
2019-06-19 12:27:01 -0600

    Added TODOs

--- rulelist/src/App.js
+++ rulelist/src/App.js
@@ -13,7 +13,8 @@ class App extends React.Component {
         { title: 'Defense', field: 'defense' , editable: 'onUpdate' },
 
         { title: 'Notes', field: 'notes', editable: 'onUpdate' },
-        
+
+          // TODO Change the names of these fields
         { title: 'Witness List', field: '1', type: 'date', editable: 'never' },
         { title: 'Scheduling Conference', field: '2', type: 'date', editable: 'never' },
         { title: 'Request PTIs', field: '3', type: 'date', editable: 'never' },
@@ -27,44 +28,68 @@ class App extends React.Component {
         { title: 'Trial', field: '11', type: 'date', editable: 'never' },
 
       ],
-      data: [
-        { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz', 
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-
-      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz', 
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-
-      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz', 
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-
-      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz', 
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-
-      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz', 
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-      ]
+      data: []
     }
   }
 
-  testFetch() {
-    return fetch("http://127.0.0.1:8000/api/deadlines/2/")
+  populateJson(json) {
+    let dataArray = [
+        { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+      },
+
+      { defendant: 'Mitch Connor', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+      },
+
+      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Joe Brown', defense: 'Lionel Hutz',
+          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+      },
+
+      { defendant: 'Billy the Kid', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+      },
+
+      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+      },
+      {
+        defendant: 'Hulk Hogan', case_number: '2019-00001', judge: 'Maury', defense: 'Saul Goodman',
+        1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+      }
+     ]
+
+    dataArray = []
+
+    json.forEach(function(x){
+      if (true) { // TODO Check if case has user assigned
+        // TODO Get deadlines for this case
+        console.log(x['defendant']); // TODO Append data to data array
+      }
+    });
+
+    this.setState(
+        {data: dataArray}
+    )
+  }
+
+  fetchJson() {
+    return fetch("http://127.0.0.1:8000/api/cases/")
           .then(response => response.json())
-          .then(json => console.log(json['datetime']))
+          .then(json => this.populateJson(json))
   }
 
   render() {
-    this.testFetch()
+    this.fetchJson() // TODO Something needs to be done with this Promise or it's just spamming a display
     return (
       
       <MaterialTable
         title="Rule List"
         columns={this.state.columns}
         data={this.state.data}
+        // TODO Set default to 10 rows or infinite
+          // TODO Remove trash can icon
+          // TODO Remove ability to add rows
         editable={{
             isDeletable: rowData => null, // no rows should be deletable
           onRowAdd: newData =>
