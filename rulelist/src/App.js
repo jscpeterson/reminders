61ea461f25129f0807f578559f52d46489698407
Joseph Peterson <jscpeterson@gmail.com>
2019-06-20 10:52:20 -0600

    Table populates with basic data

--- rulelist/src/App.js
+++ rulelist/src/App.js
@@ -10,60 +10,65 @@ class App extends React.Component {
         { title: 'Defendant', field: 'defendant', editable: 'never' },
         { title: 'CR#', field: 'case_number', editable: 'never' },
         { title: 'Judge', field: 'judge' , editable: 'onUpdate'},
-        { title: 'Defense', field: 'defense' , editable: 'onUpdate' },
+        { title: 'Defense', field: 'defense_attorney' , editable: 'onUpdate' },
 
         { title: 'Notes', field: 'notes', editable: 'onUpdate' },
 
-          // TODO Change the names of these fields
-        { title: 'Witness List', field: '1', type: 'date', editable: 'never' },
-        { title: 'Scheduling Conference', field: '2', type: 'date', editable: 'never' },
-        { title: 'Request PTIs', field: '3', type: 'date', editable: 'never' },
-        { title: 'Conduct PTIs', field: '4', type: 'date', editable: 'never' },
-        { title: 'Witness PTIs', field: '5', type: 'date', editable: 'never' },
-        { title: 'Scientific Evidence', field: '6', type: 'date', editable: 'never' },
-        { title: 'Pretrial Conference', field: '7', type: 'date', editable: 'never' },
-        { title: 'Final Witness List', field: '8', type: 'date', editable: 'never' },
-        { title: 'Need for Interpreter', field: '9', type: 'date', editable: 'never' },
-        { title: 'Plea Agreement', field: '10', type: 'date', editable: 'never' },
-        { title: 'Trial', field: '11', type: 'date', editable: 'never' },
+        { title: 'Witness List', field: 'witness_list', type: 'date', editable: 'never' },
+        { title: 'Scheduling Conference', field: 'scheduling_conf', type: 'date', editable: 'never' },
+        { title: 'Request PTIs', field: '3', type: 'request_pti', editable: 'never' },
+        { title: 'Conduct PTIs', field: '4', type: 'conduct_pti', editable: 'never' },
+        { title: 'Witness PTIs', field: '5', type: 'witness_pti', editable: 'never' },
+        { title: 'Scientific Evidence', field: 'scientific_evidence', type: 'date', editable: 'never' },
+        { title: 'Pretrial Motion Filing', field: 'pretrial_motion_filing', type: 'date', editable: 'never' },
+        { title: 'Pretrial Conference', field: 'pretrial_conf', type: 'date', editable: 'never' },
+        { title: 'Final Witness List', field: 'final_witness_list', type: 'date', editable: 'never' },
+        { title: 'Need for Interpreter', field: 'need_for_interpreter', type: 'date', editable: 'never' },
+        { title: 'Plea Agreement', field: 'plea_agreement', type: 'date', editable: 'never' },
+        { title: 'Trial', field: '11', type: 'trial', editable: 'never' },
 
       ],
       data: []
     }
   }
 
-  populateJson(json) {
-    let dataArray = [
-        { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
+  populateJson(jsonArray) {
+    // let dataArray = [
+    //     { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+    //       1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+    //   },
+    //
+    //   { defendant: 'Mitch Connor', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+    //       1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+    //   },
+    //
+    //   { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Joe Brown', defense: 'Lionel Hutz',
+    //       1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+    //   },
+    //
+    //   { defendant: 'Billy the Kid', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+    //       1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+    //   },
+    //
+    //   { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
+    //       1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+    //   },
+    //   {
+    //     defendant: 'Hulk Hogan', case_number: '2019-00001', judge: 'Maury', defense: 'Saul Goodman',
+    //     1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
+    //   }
+    //  ]
 
-      { defendant: 'Mitch Connor', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
+    let dataArray = []
 
-      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Joe Brown', defense: 'Lionel Hutz',
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-
-      { defendant: 'Billy the Kid', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-
-      { defendant: 'Scruff McGruff', case_number: '2019-00000', judge: 'Judy', defense: 'Lionel Hutz',
-          1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      },
-      {
-        defendant: 'Hulk Hogan', case_number: '2019-00001', judge: 'Maury', defense: 'Saul Goodman',
-        1: '2019/07/04', 2:'2019/07/04', 3: '2019/07/04', 4:'2019/07/04', 5:'2019/07/04', 6:'2019/07/04', 7:'2019/07/04' ,8: '2019/07/04',9: '2019/07/04',10:'2019/07/04', 11:'2019/07/04',
-      }
-     ]
-
-    // dataArray = []
-
-    json.forEach(function(x){
-        // TODO Get deadlines for this case
-        console.log(x); // TODO Append data to data array
+    jsonArray.forEach(function(json){
+        let row = {};
+        row['defendant'] = json['defendant'];
+        row['case_number'] = json['case_number'];
+        row['judge'] = json['judge']; // TODO Need to get correct judge
+        row['defense_attorney'] = json['defense_attorney'];
+        row['notes'] = json['notes'];
+        dataArray.push(row);
     });
 
     this.setState(
@@ -74,7 +79,7 @@ class App extends React.Component {
   fetchJson() {
     return fetch("http://127.0.0.1:8000/api/cases/")
           .then(response => response.json())
-          .then(json => this.populateJson(json))
+          .then(jsonArray => this.populateJson(jsonArray))
   }
 
   componentDidMount() {
