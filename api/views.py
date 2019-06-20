75711c11fd961fa0745b10aac16c9b354477236c
Joseph Peterson <jscpeterson@gmail.com>
2019-06-20 14:19:10 -0600

    Deadlines can be filtered by query params

--- api/views.py
+++ api/views.py
@@ -1,7 +1,5 @@
 from django.db.models import Q
-from django.shortcuts import render
 from rest_framework import viewsets
-
 from api.serializers import DeadlineSerializer, CaseSerializer
 from remind.models import Deadline, Case
 
@@ -11,11 +9,20 @@ class DeadlineViewSet(viewsets.ModelViewSet):
     queryset = Deadline.objects.all()
 
     def get_queryset(self):
-        return super().get_queryset().filter(
-            Q(case__supervisor=self.request.user) |
-            Q(case__prosecutor=self.request.user) |
-            Q(case__secretary=self.request.user)
-        )
+        case_pk = self.request.query_params.get('case', None)
+
+        if case_pk is None:
+            return super().get_queryset().filter(
+                Q(case__supervisor=self.request.user) |
+                Q(case__prosecutor=self.request.user) |
+                Q(case__secretary=self.request.user)
+            )
+        else:
+            return super().get_queryset().filter(case=case_pk).filter(
+                Q(case__supervisor=self.request.user) |
+                Q(case__prosecutor=self.request.user) |
+                Q(case__secretary=self.request.user)
+            )
 
 
 class CaseViewSet(viewsets.ModelViewSet):
