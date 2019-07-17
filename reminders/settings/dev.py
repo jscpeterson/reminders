3eae558a3b358f13153554a3d1233300fef56721
Joseph Peterson <jscpeterson@gmail.com>
2019-07-17 07:32:22 -0600

    Removed debug toolbar from dev

--- reminders/settings/dev.py
+++ reminders/settings/dev.py
@@ -25,9 +25,9 @@ INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
-MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
-]
+# MIDDLEWARE += [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware'
+# ]
 
 INTERNAL_IPS = [
     '127.0.0.1',
