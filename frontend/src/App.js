f8c6b986d32dfaf01ca3115a513d421bddfb9af1
Joseph Peterson <jscpeterson@gmail.com>
2019-07-23 14:20:32 -0600

    ManagementTable Tab loads for supervisors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,56 @@ import Tab from 'muicss/lib/react/tab';
 
 import RuleList from './RuleList'
 import UpcomingDeadlines from './UpcomingDeadlines'
+import ManagementTable from './ManagementTable'
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <Tabs justified={true}>
-        <Tab value="pane-1" label="Rule List">
-          <RuleList/>
-        </Tab>
-        <Tab value="pane-2" label="Upcoming Deadlines">
-          <UpcomingDeadlines/>
-        </Tab>
-      </Tabs>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            position:0
+        }
+    }
+
+    fetchUser() {
+        return fetch("/api/user/")
+            .then(response => response.json())
+            .then(userData => this.saveUserData(userData))
+    }
+
+    saveUserData(userData) {
+        this.setState({position:userData[0]['position']})
+    }
+
+    componentDidMount() {
+        this.fetchUser();
+    }
+
+    renderTab() {
+        return <Tab value="pane-2" label="Management">
+                    <ManagementTable/>
+               </Tab>
+    }
+
+    render() {
+        let tab;
+        const SUPERVISOR = 1;
+
+        if (this.state.position === SUPERVISOR) {
+            tab = this.renderTab();
+        }
+
+        return (
+            <Tabs justified={true}>
+                <Tab value="pane-1" label="Rule List">
+                    <RuleList/>
+                </Tab>
+                <Tab value="pane-2" label="Upcoming Deadlines">
+                    <UpcomingDeadlines/>
+                </Tab>
+                { tab }
+            </Tabs>
     )
   }
 }
---
+++ frontend/src/ManagementTable.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class ManagementTable extends React.Component {
+
+    render() {
+        return <p>Management Table Goes Here</p>
+    }
+
+}
+
+export default ManagementTable;
