a286e02e4e0b739ba47734090b82e323ab9f7209
Joseph Peterson <jscpeterson@gmail.com>
2019-09-05 14:46:46 -0600

    Created management command for supervisor flag

---
+++ remind/management/commands/change_supervisor.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from users.models import CustomUser
+
+
+class Command(BaseCommand):
+    help = 'Changes supervisor privileges for a user.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--user', type=str, required=True)
+        parser.add_argument('--supervisor', type=bool, required=True)
+
+    def handle(self, *args, **kwargs):
+        first_name = kwargs['user'].split(' ')[0]
+        last_name = kwargs['user'].split(' ')[1]
+
+        # Check user exists
+        if not CustomUser.objects.filter(first_name=first_name, last_name=last_name).exists():
+            raise Exception('{first_name} {last_name} not recognized as a user.'.format(
+                first_name=first_name,
+                last_name=last_name,
+            ))
+
+        user = CustomUser.objects.filter(first_name=first_name, last_name=last_name).exists()
+
+        if kwargs['supervisor']:
+            if user.is_supervisor:
+                print('{} is already a supervisor.'.format(user))
+            else:
+                user.is_supervisor = True
+                user.save(update_fields=['is_supervisor'])
+                print('Supervisor privileges have been given to {}.'.format(user))
+        elif not kwargs['supervisor']:
+            if user.is_supervisor:
+                print('{} is not a supervisor.'.format(user))
+            else:
+                user.is_supervisor = False
+                user.save(update_fields=['is_supervisor'])
+                print('Supervisor privileges have been removed from {}.'.format(user))
