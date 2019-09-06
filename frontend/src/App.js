bc02a78a237b1927682b06ede3f0874c0044746a
Joseph Peterson <jscpeterson@gmail.com>
2019-09-06 13:45:39 -0600

    Dashboard displays additional tables if user is supervisor or developer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,65 @@ import UpcomingDeadlines from './UpcomingDeadlines'
 
 class App extends React.Component {
 
+  constructor(props) {
+       super(props);
+
+       this.state = {
+            position: 0,
+            is_supervisor: false
+       }
+  }
+
+  fetchUser() {
+       return fetch("/api/user/")
+            .then(response => response.json())
+            .then(userData => this.saveUserData(userData))
+  }
+
+    saveUserData(userData) {
+        this.setState({
+            position:userData[0]['position'],
+            is_supervisor:userData[0]['is_supervisor']
+        })
+    }
+
+    componentDidMount() {
+        this.fetchUser();
+    }
+
+    renderStaffRuleList() {
+        return <Tab value="pane-3" label="Staff Rule List">
+                    <RuleList/>
+               </Tab>
+    }
+
+    renderStaffDeadlines() {
+        return <Tab value="pane-4" label="Staff Deadlines">
+                    <UpcomingDeadlines/> 
+               </Tab>
+    }
+
   render() {
-    return (
+      const DEVELOPER = 99;
+      let staffRuleList;
+      let staffDeadlines;
+
+      if (this.state.is_supervisor || this.state.position === DEVELOPER) {
+          staffRuleList = this.renderStaffRuleList();
+          staffDeadlines = this.renderStaffDeadlines();
+      }
+
+
+      return (
       <Tabs justified={true}>
         <Tab value="pane-1" label="Rule List">
           <RuleList/>
         </Tab>
+          { staffRuleList }
         <Tab value="pane-2" label="Upcoming Deadlines">
           <UpcomingDeadlines/>
         </Tab>
+          { staffDeadlines }
       </Tabs>
     )
   }
